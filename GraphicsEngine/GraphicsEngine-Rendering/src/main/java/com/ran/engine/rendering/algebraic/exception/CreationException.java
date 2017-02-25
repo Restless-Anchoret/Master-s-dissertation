3dd058bc151389e05dfb20a40f86f16b97dfe868
RestlessAnchoret <et.anchoret@gmail.com>
2017-02-25 11:20:46 +0300

    https://trello.com/c/Yc1YaTuI Prepare Rendering module with all necessary changes DrawingMechanism module was renamed to Rendering module. First old classes were added to this module.

---
+++ GraphicsEngine/GraphicsEngine-Rendering/src/main/java/com/ran/engine/rendering/algebraic/exception/CreationException.java
@@ -0,0 +1,13 @@
+package com.ran.engine.rendering.algebraic.exception;
+
+public class CreationException extends AlgebraicException {
+
+    public CreationException(String message) {
+        super(message);
+    }
+
+    public CreationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
