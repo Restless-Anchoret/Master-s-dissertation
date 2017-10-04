6a7ccb53fee7d6297ead22b777e0e0b25891f9dc
Ilya Kuznetsov <et.anchoret@gmail.com>
2017-10-04 19:36:01 +0300

    PlaneBezierCurveCreator was implemented.

--- GraphicsEngine/GraphicsEngine-Factories/src/main/java/com/ran/engine/factories/interpolation/curvecreators/PlaneBezierCurveCreator.java
+++ GraphicsEngine/GraphicsEngine-Factories/src/main/java/com/ran/engine/factories/interpolation/curvecreators/PlaneBezierCurveCreator.java
@@ -1,14 +1,12 @@
 package com.ran.engine.factories.interpolation.curvecreators;
 
+import com.ran.engine.algebra.function.DoubleFunction;
+import com.ran.engine.algebra.function.DoubleMultifunction;
+import com.ran.engine.algebra.vector.TwoDoubleVector;
 import com.ran.engine.factories.interpolation.input.SimpleInputParameters;
 import com.ran.engine.factories.interpolation.tools.CurvesSmoothingCreator;
 import com.ran.engine.factories.interpolation.tools.SegmentsBuilder;
 import com.ran.engine.factories.interpolation.tools.TimeMomentsUtil;
-import com.ran.engine.algebra.function.DoubleFunction;
-import com.ran.engine.algebra.matrix.DoubleMatrix;
-import com.ran.engine.algebra.vector.DoubleVector;
-import com.ran.engine.algebra.vector.ThreeDoubleVector;
-import com.ran.engine.algebra.vector.TwoDoubleVector;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,27 +42,25 @@ public class PlaneBezierCurveCreator extends AbstractPlainCurveCreator {
             ));
         }
 
-        List<TwoDoubleVector> halfSegmentsForward = new ArrayList<>(k - 1);
-        List<TwoDoubleVector> halfSegmentsBack = new ArrayList<>(k - 1);
+        List<DoubleFunction<TwoDoubleVector>> halfSegmentsForward = new ArrayList<>(k - 1);
+        List<DoubleFunction<TwoDoubleVector>> halfSegmentsBack = new ArrayList<>(k - 1);
         for (int i = 0; i < k - 1; i++) {
-//            BigArcsBuilder.Result firstHalfBigArcsBuilderResult = segmentsBuilder.buildBigArcBetweenVerticesOnSphere(
-//                    vertices.get(i), segmentsCenters.get(i));
-//            halfSegmentsForward.add(firstHalfBigArcsBuilderResult.getRotation());
-//            BigArcsBuilder.Result secondHalfBigArcsBuilderResult = segmentsBuilder.buildBigArcBetweenVerticesOnSphere(
-//                    vertices.get(i + 1), segmentsCenters.get(i));
-//            halfSegmentsBack.add(secondHalfBigArcsBuilderResult.getRotation());
+            SegmentsBuilder.Result firstHalfSegmentsBuilderResult = segmentsBuilder
+                    .buildSegment(vertices.get(i), segmentsCenters.get(i));
+            halfSegmentsForward.add(firstHalfSegmentsBuilderResult.getSegment());
+            SegmentsBuilder.Result seconfHalfSegmentsBuilderResult = segmentsBuilder
+                    .buildSegment(vertices.get(i + 1), segmentsCenters.get(i));
+            halfSegmentsBack.add(seconfHalfSegmentsBuilderResult.getSegment());
         }
 
-        List<DoubleFunction<DoubleMatrix>> smoothedRotations = new ArrayList<>(k);
-//        smoothedRotations.add(halfSegmentsForward.get(0));
-//        for (int i = 1; i < k - 1; i++) {
-//            smoothedRotations.add(curvesSmoothingCreator.smoothCurves(
-//                    halfSegmentsBack.get(i - 1), halfSegmentsForward.get(i), degree));
-//        }
-//        smoothedRotations.add(new DoubleFunction<>(
-//                point -> halfSegmentsBack.get(k - 2).apply(1.0 - point),
-//                0.0, 1.0)
-//        );
+        List<DoubleFunction<TwoDoubleVector>> smoothedSegments = new ArrayList<>(k);
+        smoothedSegments.add(halfSegmentsForward.get(0));
+        for (int i = 1; i < k - 1; i++) {
+            smoothedSegments.add(curvesSmoothingCreator.smoothCurves(
+                    halfSegmentsBack.get(i - 1), halfSegmentsForward.get(i), degree));
+        }
+        smoothedSegments.add(new DoubleFunction<>(
+                point -> halfSegmentsBack.get(k - 2).apply(1.0 - point)));
 
         List<Double> timeMoments = new ArrayList<>(k + 1);
         double timeDelta = t1 - t0;
@@ -72,21 +68,16 @@ public class PlaneBezierCurveCreator extends AbstractPlainCurveCreator {
             timeMoments.add(t0 + i * timeDelta);
         }
 
-        List<DoubleFunction<ThreeDoubleVector>> curveSegments = new ArrayList<>(k);
+        List<DoubleFunction<TwoDoubleVector>> curveSegments = new ArrayList<>(k);
         for (int i = 0; i < k; i++) {
             double startTime = timeMoments.get(i);
             double endTime = timeMoments.get(i + 1);
-            DoubleFunction<DoubleMatrix> currentRotation = smoothedRotations.get(i);
-            DoubleVector currentVertice = vertices.get(i).getDoubleVector();
-            DoubleFunction<ThreeDoubleVector> curveSegmentWithoutAligning = new DoubleFunction<>(
-                    point -> new ThreeDoubleVector(currentRotation.apply(point).multiply(currentVertice)), 0.0, 1.0
-            );
-            DoubleFunction<ThreeDoubleVector> alignedCurveSegment =
-                    curveSegmentWithoutAligning.superposition(timeMomentsUtil.buildAligningFunction(startTime, endTime));
+            DoubleFunction<TwoDoubleVector> currentCurve = smoothedSegments.get(i);
+            DoubleFunction<TwoDoubleVector> alignedCurveSegment =
+                    currentCurve.superposition(timeMomentsUtil.buildAligningFunction(startTime, endTime));
             curveSegments.add(alignedCurveSegment);
         }
-//        return DoubleMultifunction.makeMultifunction(curveSegments);
-        return null;
+        return DoubleMultifunction.makeMultifunction(curveSegments);
     }
 
 }
